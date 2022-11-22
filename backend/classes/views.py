370dbdb229fe0018f3dd3aba7f760620540ac5ce
Jasmine Zhuang <jasmine.zhuang@mail.utoronto.ca>
2022-11-21 20:06:02 -0500

    change max_page_size

--- backend/classes/views.py
+++ backend/classes/views.py
@@ -252,7 +252,7 @@ class DropClassView(DestroyAPIView):
 class ClassInstancePagination(PageNumberPagination):
     page_size = 1
     page_size_query_param = 'page_size'
-    max_page_size = 2
+    max_page_size = 1
 
 
 class UserEnrollmentHistoryListView(ListAPIView):
